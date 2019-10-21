8e836979b711166620232a8e0231140836823a93
captproton <rocketeer.captproton@gmail.com>
2019-10-20 19:37:26 -0700

    ADDED: services section and copyright, privacy, and terms pages

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,16 @@
     <h1>Welcome!</h1>
     <p><nuxt-link to="/product">Product page</nuxt-link></p>
     <p><nuxt-link to="/contact">Contact page</nuxt-link></p>
+    <p><nuxt-link to="/services/talent">Talent page</nuxt-link></p>
+    <p><nuxt-link to="/services/influencers">Infulencers page</nuxt-link></p>
+    <p><nuxt-link to="/services/experience">experience page</nuxt-link></p>
+    <p><nuxt-link to="/services/4ds">4ds page</nuxt-link></p>
+    <p><nuxt-link to="/services/production">production page</nuxt-link></p>
+    <p><nuxt-link to="/services/ecommerce">ecommerce page</nuxt-link></p>
+    <p><nuxt-link to="/services/paid-media">paid-media page</nuxt-link></p>
+    <p><nuxt-link to="/services/strategy">strategy page</nuxt-link></p>
+    <p><nuxt-link to="/services/case-studies">case-studies page</nuxt-link></p>
+    <p><nuxt-link to="/services/">services page</nuxt-link></p>
   </div>
 </template>
 
