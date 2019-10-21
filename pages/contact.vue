ae4a5095f3c8a51e0bd0093163067138aeb75ab8
captproton <rocketeer.captproton@gmail.com>
2019-10-20 18:42:18 -0700

    WIP: transitions

---
+++ pages/contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>This is the contact page</h1>
+    <p><nuxt-link to="/">Home page</nuxt-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  transition: 'fadeOpacity'
+}
+</script>
+
+<style>
+.fadeOpacity-enter-active, .fadeOpacity-leave-active {
+  transition: opacity .35s ease-out;
+}
+
+.fadeOpacity-enter, .fadeOpacity-leave-active {
+  opacity: 0;
+}
+</style>
