ab7e79b25b4b07d653d9af8ab0b795192f44a2d3
captproton <rocketeer.captproton@gmail.com>
2019-10-24 11:20:29 -0700

    CHANGE: improved layout of services index with css grid

--- pages/services/index.vue
+++ pages/services/index.vue
@@ -1,57 +1,112 @@
 <template>
   <div class="container">
-  	<div class="page-headline theme-services">
-    <div class="container">
-        <h1 class="text-center text-uppercase">Services</h1>
-        <p>As a full-service digital agency built for the now,<br>
-we’ve grown our practices on what will drive results in today’s mobile landscape:</p>
-    </div>
-</div>
-    <h1>This is the services page</h1>
-    <p><nuxt-link to="/">Home page</nuxt-link></p>
-    <a href="talent">talent page</a>
-    <a href="influencers">influencers page</a>
-    <a href="case-studies">case studies</a>
+  	<div class="wrapper">
+	  <header>Header</header>
+	  <main>
+				<div class="box service-summary">
+			
+					<div class="box service-summary-icon">
+						                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/">
+                            <img src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2019/01/29010712/creative-blk.png" class="img-responsive">
+                        </a>
 
-  <div class="strategy">
-  	<a href="strategy">stategy</a>
-  </div>
-  <div class="experience"> 
-  	<a href="experience">experience</a>   
-    <p><nuxt-link to="/services/experience">experience page</nuxt-link></p>
-  </div>
-  <div class="paid-media">
-  	      	<a href="paid-media">paid-media</a>   
+					</div>
+					<div class="box service-summary-info">
+						                        <h4 class="service-summary-title">Creative</h4>
+                        <p>Over 200 writers, designers, photographers, editors and animators with one common goal: blowing business objectives (and minds) out of the water.</p>
+                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/"><strong>Learn More &gt;</strong></a>
 
-  </div>
-  <div class="ecommerce">
-  	      	<a href="ecommerce">ecommerce</a>   
+					</div>
+				</div>
 
-  </div>
-  <div class="production">
-  	      	<a href="production">production</a>   
+				<div class="box service-summary">
+			
+					<div class="box service-summary-icon">
+						                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/">
+                            <img src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2019/01/29010712/creative-blk.png" class="img-responsive">
+                        </a>
 
-  </div>
-  <div class="4ds">    
-  	  <a href="4ds">4ds</a>   
+					</div>
+					<div class="box service-summary-info">
+						                        <h4 class="service-summary-title">Creative</h4>
+                        <p>Over 200 writers, designers, photographers, editors and animators with one common goal: blowing business objectives (and minds) out of the water.</p>
+                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/"><strong>Learn More &gt;</strong></a>
 
-  </div>
+					</div>
+				</div>
 
-<!-- css grid start -->
-<section class="grid-1">
-  <div class="item-1">1</div>
-  <div class="item-2">2</div>
-  <div class="item-3">3</div>
-  <div class="item-4">4</div>
-  <div class="item-5">5</div>
-  <div class="item-6">6</div>
-  <div class="item-7">7</div>
-  <div class="item-8">8</div>
-  <div class="item-9">9</div>
-</section><!-- cssgrid finish -->
+	  </main>
+	  <aside>
+				<div class="box service-summary">
+			
+					<div class="box service-summary-icon">
+						                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/">
+                            <img src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2019/01/29010712/creative-blk.png" class="img-responsive">
+                        </a>
 
-  </div>
+					</div>
+					<div class="box service-summary-info">
+						                        <h4 class="service-summary-title">Creative</h4>
+                        <p>Over 200 writers, designers, photographers, editors and animators with one common goal: blowing business objectives (and minds) out of the water.</p>
+                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/"><strong>Learn More &gt;</strong></a>
 
+					</div>
+				</div>
+
+				<div class="box service-summary">
+			
+					<div class="box service-summary-icon">
+						                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/">
+                            <img src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2019/01/29010712/creative-blk.png" class="img-responsive">
+                        </a>
+
+					</div>
+					<div class="box service-summary-info">
+						                        <h4 class="service-summary-title">Creative</h4>
+                        <p>Over 200 writers, designers, photographers, editors and animators with one common goal: blowing business objectives (and minds) out of the water.</p>
+                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/"><strong>Learn More &gt;</strong></a>
+
+					</div>
+				</div>
+
+				<div class="box service-summary">
+			
+					<div class="box service-summary-icon">
+						                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/">
+                            <img src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2019/01/29010712/creative-blk.png" class="img-responsive">
+                        </a>
+
+					</div>
+					<div class="box service-summary-info">
+						                        <h4 class="service-summary-title">Creative</h4>
+                        <p>Over 200 writers, designers, photographers, editors and animators with one common goal: blowing business objectives (and minds) out of the water.</p>
+                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/"><strong>Learn More &gt;</strong></a>
+
+					</div>
+				</div>
+
+				<div class="box service-summary">
+			
+					<div class="box service-summary-icon">
+						                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/">
+                            <img src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2019/01/29010712/creative-blk.png" class="img-responsive">
+                        </a>
+
+					</div>
+					<div class="box service-summary-info">
+						                        <h4 class="service-summary-title">Creative</h4>
+                        <p>Over 200 writers, designers, photographers, editors and animators with one common goal: blowing business objectives (and minds) out of the water.</p>
+                        <a class="animsition-link" href="https://vaynermedia.com/service/creative/"><strong>Learn More &gt;</strong></a>
+
+					</div>
+				</div>
+      </aside>
+	  <footer>Footer</footer>
+  		
+  	</div>
+
+
+ </div>
 
 </template>
 
@@ -62,67 +117,55 @@ export default {
 </script>
 
 <style>
+	header {  grid-area: header;  }
+	main   {  grid-area: content; }
+	aside  {  grid-area: sidebar; }
+	footer {  grid-area: footer;  }
 
-body {
-  padding-top: 20px;
-  background: #f5f7f8;
-}
+	.wrapper {
+	  display: grid;
+	  grid-gap: 2rem;
+	  grid-template-areas: 
+	    "header"
+	    "content"
+	    "sidebar"
+	    "footer" ;
+	}
 
-.grid-1 {
-  display: grid;
-  background: #222;
-  width: 100%;
-  max-width: 750px;
-  margin: 0 auto;
-  
-  grid-template-columns: repeat(4, 1fr);
-  
-  grid-gap: 20px;
-  
-}
+	@media (min-width: 800px) {
+		.wrapper {
+			grid-template-columns: 1fr 1fr;
+			grid-template-areas: 
+		  	"header header"
+		  	"content sidebar"
+		  	"footer footer" ;
+		}
 
-/* items */
+	}
 
-.grid-1 div {
-  color: white;
-  font-size: 20px;
-  padding: 20px;
-  }
 
-/* specific item styles */
+	header, main, aside, footer {
+	  background: pink;
+	  font-size: 2rem;
+	  font-family: Avenir, Roboto, Helveltica, san-serif;
+	  padding: 2rem;
+	  text-align: center;
+	}
 
-.item-1 {
-  background: #b03532;
-}
-.item-2 {
-  background: #33a8a5;
-}
-.item-3 {
-  background: #30997a;
-}
-.item-4 {
-  background: #6a478f;
-}
-.item-5 {
-  background: #da6f2b;
-}
-.item-6 {
-  background: #3d8bb1;
-}
-.item-7 {
-  background: #e03f3f;
-}
-.item-8 {
-  background: #59a627;
-}
-.item-9 {
-  background: #4464a1;
-}
-.fadeOpacity-enter-active, .fadeOpacity-leave-active {
-  transition: opacity .35s ease-out;
-}
+	.service-summary{
+		grid-column: col 3 / span 2;
+		display: grid;
+    	grid-gap: 10px;
+		grid-template-columns: 1fr 2fr;
+	}
 
-.fadeOpacity-enter, .fadeOpacity-leave-active {
-  opacity: 0;
-}
+	.service-summary-icon {
+		grid-column: 1;
+	}
+
+	.service-summary-info {
+		grid-column: 2;
+	}
+
+
 </style>
