d7e98f6810a2cc327f1db26273bc3aa6c99d6607
captproton <rocketeer.captproton@gmail.com>
2019-11-03 12:28:59 -0800

    CHANGE: updated Footer.vue; ADDED: pages/case-studies.vue

---
+++ pages/case-studies.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <p><nuxt-link to="/">Home page</nuxt-link></p>
+        <hr>
+        <h1 class="text-center text-uppercase">Case Studies</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
