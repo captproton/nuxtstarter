0cbb5ada7ce581e673c0e4bdeae666fb0a8ce5d1
captproton <rocketeer.captproton@gmail.com>
2019-11-03 12:49:20 -0800

    ADDED: services/creative.vue

---
+++ pages/services/creative.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="page-headline">
+                <div class="service-summary-icon">
+                        <img src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2019/01/29010712/creative-blk.png" class="img-responsive">
+                </div>
+                <div class="text-center">
+                    <h4 class="text-pink text-uppercase">Service</h4>
+                    <h1 class="xl">Creative</h1>
+                </div>
+            </div>
+        </div>
+
+        <article class="container single-post service-content">
+            <div class="row">
+                <div class="col-sm-4">
+                    <blockquote class="manifesto manifesto-pink">
+                        <h5 class="text-uppercase">Manifesto</h5>
+                        Over 200 writers, designers, photographers, editors and animators with one common goal: blowing business objectives (and minds) out of the water.            </blockquote>
+                </div>
+                <div class="col-sm-8 single-post-content theme-services">
+                    <h2>Approach</h2>
+        <p>We have one goal – to create content that works. And today, that can mean a whole lot of things in addition to simply meeting business objectives. Does it work within the budget? Will it work within the timeline? Does it work for target segmentation? What about the platform? And did we mention the budget?</p>
+        <p>Brands today come with no shortage of guardrails. And that’s perfectly fine by us. We believe the smaller the box, the bigger the idea.</p>
+        <p>Our combined creative and production team consists of over 200 writers, designers, art directors, editors, animators, directors, and producers – all working to produce quality content for virtually every channel at the rapid speed of culture. Everything from Super Bowl commercials to Snapchat filters.</p>
+        <p>Our seamless integration with media ensures that we not only make the best content possible but that it’s backed by laser focus targeting, instant analytics and absolutely, positively no ad waste.</p>
+                </div>
+            </div>
+        </article>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
