0fd815503ad7f42c3ec4862fc8233f9d9eef81eb
captproton <rocketeer.captproton@gmail.com>
2019-11-03 17:46:12 -0800

    CHANGE: updated privacy policy

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'nuxtstarter',
+    title: 'Dice Media Group',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
