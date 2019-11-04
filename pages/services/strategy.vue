a5420391bbbab89c05ce33a201894c39227f755a
captproton <rocketeer.captproton@gmail.com>
2019-11-03 16:22:00 -0800

    CHANGE: content and layout of service pages ecommerce, paid-media, and strategy

--- pages/services/strategy.vue
+++ pages/services/strategy.vue
@@ -1,9 +1,37 @@
 <template>
-  <div class="container">
-    <h1>This is the strategy page</h1>
-    <p><nuxt-link to="/">Home page</nuxt-link></p>
-  </div>
-</template>
+    <main>
+        <div class="headroom">&nbsp;</div>
+        <div class="page-headline">
+
+        <div class="service-summary-icon">
+                <img src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2019/01/29010743/strategy-blk.png" class="img-responsive">
+        </div>
+        <div class="text-center">
+            <h4 class="text-pink text-uppercase">Service</h4>
+            <h1 class="xl">Strategy</h1>
+        </div>
+            </div>
+
+        <article class="container single-post service-content">
+            <div class="row">
+                <div class="caption">
+                  <blockquote class="manifesto manifesto-pink">
+                    <h5 class="text-uppercase">Manifesto</h5>
+                      With data capabilities of a media agency coupled with the cultural analysis of a creative-driven research firm, we bring companies closer to their customers.            
+                  </blockquote>
+                </div>
+                <div class="col-sm-8 single-post-content theme-services">
+                    <h2>Approach</h2>
+                    <p>We have one goal – to create content that works. And today, that can mean a whole lot of things in addition to simply meeting business objectives. Does it work within the budget? Will it work within the timeline? Does it work for target segmentation? What about the platform? And did we mention the budget?</p>
+                    <p>Brands today come with no shortage of guardrails. And that’s perfectly fine by us. We believe the smaller the box, the bigger the idea.</p>
+                    <p>Our combined creative and production team consists of over 200 writers, designers, art directors, editors, animators, directors, and producers – all working to produce quality content for virtually every channel at the rapid speed of culture. Everything from Super Bowl commercials to Snapchat filters.</p>
+                    <p>Our seamless integration with media ensures that we not only make the best content possible but that it’s backed by laser focus targeting, instant analytics and absolutely, positively no ad waste.</p>
+                </div>
+            </div>
+        </article>
+
+    </main>
+  </template>
 
 <script>
 export default {
