16846c5140c270aafe0c4432e3c5f3d256776f7a
captproton <rocketeer.captproton@gmail.com>
2019-11-05 11:06:46 -0800

    FIX: mixed content for security.  changed http:// to https://

--- pages/services/experience.vue
+++ pages/services/experience.vue
@@ -32,14 +32,14 @@
             <p>Culture Hacking and Deliberate Serendipity.</p>
 <h1><strong>Our Services</strong></h1>
 <div class="holder">
-<div class="col-xs-4 exp-image"><img class="alignnone size-medium wp-image-821" src="http://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/08/03194050/Event-Strategy-250x250.png" alt=""></div>
+<div class="col-xs-4 exp-image"><img class="alignnone size-medium wp-image-821" src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/08/03194050/Event-Strategy-250x250.png" alt=""></div>
 <div class="col-xs-8">
 <p><strong>EVENT STRATEGY</strong>We’ll decipher the event landscape and provide recommendations on where you should and shouldn’t be based on business objectives.</p>
 </div>
 </div>
 <div class="holder">
 <div class="col-xs-4 exp-image">
-<h3><img class="alignnone size-medium wp-image-822" src="http://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/08/03194150/Creative-Concepting-250x250.png" alt=""></h3>
+<h3><img class="alignnone size-medium wp-image-822" src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/08/03194150/Creative-Concepting-250x250.png" alt=""></h3>
 </div>
 <div class="col-xs-8">
 <p><strong>CREATIVE CONCEPTING</strong>The most successful events are those that are built for social. We believe our depth of knowledge in social behaviors puts us in the best position to create a shareable event.</p>
@@ -47,21 +47,21 @@
 </div>
 <div class="holder">
 <div class="col-xs-4 exp-image">
-<h3><img class="alignnone size-medium wp-image-823" src="http://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/08/03194243/Culture-Hacking-250x250.png" alt=""></h3>
+<h3><img class="alignnone size-medium wp-image-823" src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/08/03194243/Culture-Hacking-250x250.png" alt=""></h3>
 </div>
 <div class="col-xs-8">
 <p><strong>CULTURE HACKING</strong>With so many brands trying to ride the trend, we’ll help you create the trend.</p>
 </div>
 </div>
 <div class="holder">
-<div class="col-xs-4 exp-image"><img class="alignnone size-medium wp-image-824" src="http://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/08/03194323/Brand-Partnerships-250x250.png" alt=""></div>
+<div class="col-xs-4 exp-image"><img class="alignnone size-medium wp-image-824" src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/08/03194323/Brand-Partnerships-250x250.png" alt=""></div>
 <div class="col-xs-8">
 <p><strong>BRAND PARTNERSHIPS</strong></p>
 <p>We’ll pinpoint who you should be aligning with, and develop your place in B2B thought leadership.</p>
 </div>
 </div>
 <div class="holder">
-<div class="col-xs-4 exp-image"><img class="alignnone size-medium wp-image-824" src="http://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/09/21200510/tickets.png" alt=""></div>
+<div class="col-xs-4 exp-image"><img class="alignnone size-medium wp-image-824" src="https://s3.amazonaws.com/vm.com-2017/wp-content/uploads/2017/09/21200510/tickets.png" alt=""></div>
 <div class="col-xs-8">
 <p><strong>UPCOMING EVENTS</strong></p>
 <p>New events are always popping up! We’d love to see you there. <a href="/events">Check it out.</a></p>
